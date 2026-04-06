061173ecbb32e28dc97c8f9e20c8d25c6875378f
homem das sombras <ysampaio200@gmail.com>
2026-04-06 14:36:30 -0300

    att

--- produtos.js
+++ produtos.js
@@ -8,5 +8,15 @@ const produtosData = [
         "nome": "Asad Elixir",
         "preco": "R$ 250,00",
         "imagem": "elixir.webp"
+    },
+    {
+        "nome": "Club de Nuit Urban Elixir",
+        "preco": "Disponível",
+        "imagem": "club-de-nuit-urban-elixir.webp"
+    },
+    {
+        "nome": "Liquid Brun",
+        "preco": "Disponível",
+        "imagem": "liquidbrun.jpg"
     }
 ];
